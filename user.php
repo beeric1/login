c851db3012c0baa46a865766628e597f93047aef
beeric1 <beeric1@users.noreply.github.com>
2018-10-03 12:23:02 +0200

    html and code structure improved

--- user.php
+++ user.php
@@ -5,11 +5,41 @@
  * Date: 03.10.2018
  * Time: 11:03
  */
-
+$user = '';
 session_start();
-?>
-<h1>Benutzerprofil</h1>
 
-<nav>
-    <?php include_once ('inc/nav.php'); ?>
-</nav>
+if($_SESSION['login']){
+    $user = $_SESSION['username'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="de-CH">
+<head>
+    <title>Anmelde-Tool: Benutzer</title>
+
+</head>
+
+<body>
+
+    <header>
+
+        <h1>Benutzerprofil</h1>
+
+
+
+    </header>
+
+    <nav>
+        <?php include_once ('inc/nav.php'); ?>
+    </nav>
+
+
+    <?php if(!empty($user)){
+
+        echo "<p> Hallo, $user </p>";
+    }  ?>
+
+</body>
+
+</html>
